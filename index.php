057169b0c589da88e025b8750d66b23be8e7d6cb
Ahmed Mohamed <132151132+ahmedmo2399@users.noreply.github.com>
2023-09-06 14:41:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+// "task1";
+// for($x=1; $x<=10; $x++)
+// {
+//  echo "$x -";
+//  };
+ 
+
+// "task3";
+// $sum = 0;
+// for($x=1; $x<=30; $x++)
+// {
+// $sum +=$x;
+// }
+// echo "The sum of the numbers 0 to 30 is $sum"."\n";
+
+
+// "task4";
+// $n = 4;
+// $x = 1;
+// for($i=1;$i<=$n-1;$i++)
+// {
+//  $x*=($i+1); 
+// }
+// echo "The factorial of  $n = $x"."\n";
+
+
+
+ // "task5";
+// $num = 1;
+// while ( $num <= 15 ) {
+
+//     print "$num -";
+//     $num += 2;
+// }
+
+
+
+// "task6";
+// $num = 0;
+// while ( $num <= 15 ) {
+//     print "$num -";
+//     $num += 2;
+// }
+
+
+
+// "task7";
+// for( $i=1; $i<=100; $i++) {
+//     if($i%3==0){
+//         echo  " $i ";
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
